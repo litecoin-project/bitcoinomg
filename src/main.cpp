305d02c6e22cb5b11c1d63f8c26369dfa7158f4a
Jeff Garzik <jgarzik@bitpay.com>
2013-07-31 08:45:49 -0400

    Minor miner fixes (hey hey it's fun to say)
    * Fix UpdateCoins() definition in main.h
    * Remove pwalletMain reference from BitcoinMiner(), as it is passed
      a wallet argument.
    
    Conflicts:
    	src/main.h
    
    Rebased-from: 18946846d5c321d2afc11445580874c9a8bb0733

--- src/main.cpp
+++ src/main.cpp
@@ -4597,7 +4597,7 @@ void static BitcoinMiner(CWallet *pwallet)
                     assert(hash == pblock->GetHash());
 
                     SetThreadPriority(THREAD_PRIORITY_NORMAL);
-                    CheckWork(pblock, *pwalletMain, reservekey);
+                    CheckWork(pblock, *pwallet, reservekey);
                     SetThreadPriority(THREAD_PRIORITY_LOWEST);
                     break;
                 }
