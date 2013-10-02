1182ea26b7f948629791952bc72d003b902f9951
Jeff Garzik <jgarzik@bitpay.com>
2013-10-02 11:19:10 -0400

    Add -disablewallet option, to disable wallet support (and BDB)
    Conflicts:
    	src/init.cpp
    
    Rebased-from: 5d4f3a1f0cbdf52257b41ea09d175c0018ad9434

--- src/init.cpp
+++ src/init.cpp
@@ -575,6 +575,7 @@ bool AppInit2(boost::thread_group& threadGroup)
     fPrintToConsole = GetBoolArg("-printtoconsole");
     fPrintToDebugger = GetBoolArg("-printtodebugger");
     fLogTimestamps = GetBoolArg("-logtimestamps");
+    bool fDisableWallet = GetBoolArg("-disablewallet", false);
 
     if (mapArgs.count("-timeout"))
     {
@@ -657,7 +658,7 @@ bool AppInit2(boost::thread_group& threadGroup)
 
     // ********************************************************* Step 5: verify wallet database integrity
 
-    if (1) {
+    if (!fDisableWallet) {
         uiInterface.InitMessage(_("Verifying wallet..."));
 
         if (!bitdb.Open(GetDataDir()))
@@ -701,7 +702,7 @@ bool AppInit2(boost::thread_group& threadGroup)
             if (r == CDBEnv::RECOVER_FAIL)
                 return InitError(_("wallet.dat corrupt, salvage failed"));
         }
-    } // (1)
+    } // (!fDisableWallet)
 
     // ********************************************************* Step 6: network initialization
 
@@ -961,7 +962,10 @@ bool AppInit2(boost::thread_group& threadGroup)
 
     // ********************************************************* Step 8: load wallet
 
-    if (1) {
+    if (fDisableWallet) {
+        uiInterface.InitMessage(_("Wallet disabled!"));
+        pwalletMain = NULL;
+    } else {
         uiInterface.InitMessage(_("Loading wallet..."));
 
         nStart = GetTimeMillis();
@@ -1048,7 +1052,7 @@ bool AppInit2(boost::thread_group& threadGroup)
             pwalletMain->SetBestChain(CBlockLocator(pindexBest));
             nWalletDBUpdated++;
         }
-    } // (1)
+    } // (!fDisableWallet)
 
     // ********************************************************* Step 9: import blocks
 
