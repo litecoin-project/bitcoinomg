0ca7ad0851b1cca1f514628eabcbbf8b2aaf9869
Mike Hearn <hearn@google.com>
2013-11-26 12:52:21 +0100

    Store and use a sanitized subVer
    Rebased-from: a946aa8d3ec7009ac670eeb65a525efe5eeb6e84

--- src/net.cpp
+++ src/net.cpp
@@ -513,7 +513,7 @@ void CNode::copyStats(CNodeStats &stats)
     X(nTimeConnected);
     X(addrName);
     X(nVersion);
-    X(strSubVer);
+    X(cleanSubVer);
     X(fInbound);
     X(nStartingHeight);
     X(nMisbehavior);
