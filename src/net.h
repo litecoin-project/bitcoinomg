858abc536ccce67b652b092f224bf3495f3bc27d
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-05-29 12:33:17 +0200

    rename fNoListen to fListen and move to net
    - better code readability and it belongs to net
    - this is a prerequisite for a pull to add -listen to the GUI
    
    Rebased-from: 53a088154ce4e1fc483afa120a9063811dd0e7ea

--- src/net.h
+++ src/net.h
@@ -108,6 +108,7 @@ CAddress GetLocalAddress(const CNetAddr *paddrPeer = NULL);
 
 
 extern bool fDiscover;
+extern bool fListen;
 extern uint64_t nLocalServices;
 extern uint64_t nLocalHostNonce;
 extern CAddrMan addrman;
